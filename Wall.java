1fcec6563dd98b380c256d86b5c6a00a00d607a1
Daniela Schafer <schaferd@winchesterthurston.org>
2017-12-16 10:18:43 -0500

    added isBlockedByWall method

--- Wall.java
+++ Wall.java
@@ -1,4 +1,24 @@
 public class Wall extends Obstacle
 {
-  
+
+  private int xStart;
+  private int yStart;
+  private int length;
+
+  public Wall(int xPos, int yPos, int len)
+  {
+    xStart = xPos;
+    yStart = yPos;
+    thickness = length;
+  }
+
+  public boolean isBlockedByWall(int xPos, int yPos)
+  {
+    if(xPosition == xPos || yPosition == yPos)
+      return true;
+    else
+      return false;
+  }
+
+
 }
