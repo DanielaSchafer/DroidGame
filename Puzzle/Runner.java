07d8ef0adeb296ea8deb90f8e843e3ead947ccc6
Daniela Schafer <schaferd@winchesterthurston.org>
2017-12-16 12:37:06 -0500

    sdf

--- Puzzle/Runner.java
+++ Puzzle/Runner.java
@@ -1,14 +1,15 @@
 public class Runner
 {
   ArrayList<Tool> tools = new ArrayList<String>();
+
 //array to hold the commands
 //change positon of the droid based on the commands
 
-  public parseTools(Droid x, ArrayList<Tool> tools){
-    for(int i = 0; i<tools.length; i++)
-    {
-      if(tools.get(i).toString.compareTo("Loop") == 0)
-      x.moveXPos(tools.get(i).)
-    }
-  }
+
+
+public parseTool(  ArrayList<Tool> tools)
+{
+  switch(page)
+  		{
+  		case HOME:
 }
