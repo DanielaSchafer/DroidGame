06dc997e17a2f1c7e681d0400ee41b358ffc03bb
Daniela Schafer <schaferd@winchesterthurston.org>
2017-12-16 11:35:46 -0500

    added methods

--- Puzzle/Droid.java
+++ Puzzle/Droid.java
@@ -1,3 +1,22 @@
 public class Droid{
 
+int xPosition;
+int yPosition;
+
+public Droid(int xPos, int yPos)
+{
+  xPosition = xPos;
+  yPosition = yPos;
+}
+
+public void moveXPos(int change)
+{
+  xPosition = xPosition+change;
+}
+
+public void moveYPos(int change)
+{
+  yPosition = yPosition+change;
+}
+
 }
