69efde0726a93648b162db5bb85523a9315dee74
Sophia Lebiere <lebieres@winchesterthurston.org>
2017-12-16 11:12:33 -0500

    loop

--- Tools/loop.java
+++ Tools/loop.java
@@ -9,37 +9,33 @@ public class Loop extends Tool()
     super();
   }
 
-  public Loop (int xPos; int yPos; int zPos)
-  {
+  public Loop (int xPos; int yPos; int zPos, int numOfRepeats){
     super(xPos; yPos; zPos);
+    repeats = numOfRepeats;
   }
 
-  public void setRepeats (int repeatIn)
-  {
-    repeats= repeatsIn;
+  public void setRepeats(int repeatIn){
+    repeats = repeatsIn;
   }
 
-  public void setString(ArrayList<String> commandsIn)
-  {
+  public int getRepeats(){
+    return repeats;
+  }
+
+  public void setString(ArrayList<String> commandsIn){
     StringBuilder builder = new StringBuilder();
-    for (int i = 0; i<commandsIn.size(); i++)
-    {
+    for (int i = 0; i<commandsIn.size(); i++){
       builder.append(commandsIn.get(i));
     }
     commands = builder.toString();
   }
 
-  public String toString()
-  {
+  public String toString(){
     //repeats the string the number of times the loop is supposed to run
     StringBuilder builder = new StringBuilder();
-    for (int i = 1; i<=repeats; i++)
-    {
+    for (int i = 1; i<=repeats; i++){
       builder.append(commands);
     }
     return builder.toString();
   }
-
-
-
 }
