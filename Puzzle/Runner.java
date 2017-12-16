9b3db546ac8cef1b73ab780d6e3b77e586e52cfd
Sophia Lebiere <lebieres@winchesterthurston.org>
2017-12-16 12:18:48 -0500

    indent runner

--- Puzzle/Runner.java
+++ Puzzle/Runner.java
@@ -4,12 +4,11 @@ public class Runner
 //array to hold the commands
 //change positon of the droid based on the commands
 
-public parseTools(Droid x, ArrayList<Tool> tools)
-{
-  for(int i = 0; i<tools.length; i++)
-  {
-    if(tools.get(i).toString.compareTo("Loop") == 0)
-    x.moveXPos(tools.get(i).)
+  public parseTools(Droid x, ArrayList<Tool> tools){
+    for(int i = 0; i<tools.length; i++)
+    {
+      if(tools.get(i).toString.compareTo("Loop") == 0)
+      x.moveXPos(tools.get(i).)
+    }
   }
-}
 }
