308efdc1a95ffc199da5110f15dd1dd719967dcd
Daniela Schafer <schaferd@winchesterthurston.org>
2017-12-16 10:02:51 -0500

    made wall class, finished obstacle class

--- Obstacle.java
+++ Obstacle.java
@@ -3,6 +3,24 @@ Public class Obstacle{
 public static void main(String[] args) {
 }
 
+private int xPosition;
+private int yPosition;
+
+public Obstacle(int xPos, int yPos)
+{
+  xPosition = xPos;
+  yPosition = yPos;
+}
+
+public int getXPos()
+{
+  return xPosition;
+}
+
+public int getYPos()
+{
+  return yPosition;
+}
 
 
 }
