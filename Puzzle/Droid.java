0c2db6b326da4548d43a699f6f948b874e7e4968
Daniela Schafer <schaferd@winchesterthurston.org>
2017-12-16 12:50:15 -0500

    sdf

--- Puzzle/Droid.java
+++ Puzzle/Droid.java
@@ -1,12 +1,14 @@
 public class Droid{
 
-int xPosition;
-int yPosition;
+private int xPosition;
+private int yPosition;
+private Direction d;
 
 public Droid(int xPos, int yPos)
 {
   xPosition = xPos;
   yPosition = yPos;
+  d = Direction.EAST;
 }
 
 public void moveXPos(int change)
@@ -14,9 +16,21 @@ public void moveXPos(int change)
   xPosition = xPosition+change;
 }
 
+public void changeDirection(Direction dir)
+{
+  d = Direction.dir;
+}
+
+public Direction getDirection()
+{
+  return d;
+}
+
 public void moveYPos(int change)
 {
   yPosition = yPosition+change;
 }
+
+public void setDirection
 
 }
