2374e82ff60aa1378532521aac71d68141daf188
Nadine Oury <Nadine@Nadines-MacBook.local>
2017-12-16 10:54:56 -0500

    loops

--- Tools/loop.java
+++ Tools/loop.java
@@ -1,6 +1,45 @@
 public class Loop extends Tool()
 {
   int [] position;
-  
+  whatever look;
+  int repeats;//number of times the loop runs
+  String commands="";
+
+  public Loop (){
+    super();
+  }
+
+  public Loop (int xPos; int yPos; int zPos)
+  {
+    super(xPos; yPos; zPos);
+  }
+
+  public void setRepeats (int repeatIn)
+  {
+    repeats= repeatsIn;
+  }
+
+  public void setString(ArrayList<String> commandsIn)
+  {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i<commandsIn.size(); i++)
+    {
+      builder.append(commandsIn.get(i));
+    }
+    commands = builder.toString();
+  }
+
+  public String toString()
+  {
+    //repeats the string the number of times the loop is supposed to run
+    StringBuilder builder = new StringBuilder();
+    for (int i = 1; i<=repeats; i++)
+    {
+      builder.append(commands);
+    }
+    return builder.toString();
+  }
+
+
 
 }
