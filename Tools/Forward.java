f6a5cdc406b65b6f4c9f5054dca30c5d493d27ad
Daniela Schafer <schaferd@winchesterthurston.org>
2017-12-16 11:42:30 -0500

    no z coordinate needed

--- Tools/Forward.java
+++ Tools/Forward.java
@@ -7,8 +7,8 @@ public class Forward extends Tool{
     super();
   }
 
-  public Forward(int xPos, int yPos, int zPos){
-    super(xPos, yPos, zPos);
+  public Forward(int xPos, int yPos){
+    super(xPos, yPos);
   }
 
   public String toString(){
